c5b2739c21d7cdd13654738598ebe79ef1f5716b
Dave Germiquet <davegermiquet@trulycanadian.net>
2017-09-09 15:37:56 +0000

    Started model structure

--- webinterfaceforpi/restapiwifi/models.py
+++ webinterfaceforpi/restapiwifi/models.py
@@ -3,4 +3,19 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class NetworkDevice(models.model):
+    device = models.CharField(max_length=100, blank=True, default='')
+    
+class WifiNetworks(models.model):
+    wifissid = models.CharField(max_length=100, blank=True, default='')
+    wifikey_mgmt = models.CharField(max_length=100, blank=True, default='')
+    wifipsk =  models.CharField(max_length=100, blank=True, default='')
+
+class TypeOfNetwork(models.model):
+    network_type =  models.CharField(max_length=100, blank=True, default='')
+
+class routes(models.model):
+    route_type =  models.CharField(max_length=100, blank=True, default='')
+
+
