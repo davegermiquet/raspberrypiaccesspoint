56e58d1e0aad23555bf8d1e549659156ac89fb2e
Dave Germiquet <davegermiquet@trulycanadian.net>
2017-09-09 15:40:20 +0000

    Fixed typo

--- webinterfaceforpi/restapiwifi/models.py
+++ webinterfaceforpi/restapiwifi/models.py
@@ -3,19 +3,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-
-class NetworkDevice(models.model):
+class NetworkDevice(models.Model):
     device = models.CharField(max_length=100, blank=True, default='')
     
-class WifiNetworks(models.model):
+class WifiNetworks(models.Model):
     wifissid = models.CharField(max_length=100, blank=True, default='')
     wifikey_mgmt = models.CharField(max_length=100, blank=True, default='')
     wifipsk =  models.CharField(max_length=100, blank=True, default='')
 
-class TypeOfNetwork(models.model):
+class TypeOfNetwork(models.Model):
     network_type =  models.CharField(max_length=100, blank=True, default='')
 
-class routes(models.model):
+class routes(models.Model):
     route_type =  models.CharField(max_length=100, blank=True, default='')
 
 
